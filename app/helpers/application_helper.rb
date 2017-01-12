bea2787af77b0171a56e360b5ab47804f7a754b2
munens <kaumbuthom@gmail.com>
2017-01-12 16:49:57 +0000

    Sec. 6, lec. 101-104: new user sign up + user and profile image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+    # to get the gravatars size the function below can take another argument - options:
+    def gravatar_for(user,  options = {size: 80})
+        # gravatar urls are based on the users hash: We will use the gravatar_id variable to do this:
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        
+        size = options[:size]
+        # append the size to the end:
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+        
+        # the image tag method can take in the url, the 'alt' and the class - which in this case we want a circle:
+        image_tag(gravatar_url, alt: user.username, class: "img-circle")
+    end
 end
